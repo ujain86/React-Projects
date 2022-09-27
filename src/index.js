51dfbfbab8c7eef347133721665440b9b50e7644
ujain86 <111032592+ujain86@users.noreply.github.com>
2022-09-27 17:50:26 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// Create a functional component called MyProject that returns the following UI:
+
+// An h1 with the name of your project
+// A paragraph to give a brief description of your project
+// An ordered or unordered list to discuss 3 major skills used in this project
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { render } from "react-dom";
+import Hello from "./hello";
+
+ReactDOM.render(<Hello />, document.getElementById("root"));
